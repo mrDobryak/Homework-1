75ecd30a583f85d7879c30fc37eec458b5f98bc3
mrDobryak <clainrus@gmail.com>
2018-11-26 17:26:03 +1000

    npm init + moment

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+console.log('\x1Bc');
+// npm start
+
+var moment = require("moment")
+
+var fmt = "DD.MM.YYYY HH:mm:ss"
+var date = moment()
+
+date.add(3, "month").endOf("month")
+
+console.log(date.format(fmt))
