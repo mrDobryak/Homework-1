9ba168820cab228f3b6e10f4d0dc3e0769db2c74
mrDobryak <clainrus@gmail.com>
2018-11-30 21:53:48 +1000

    Вывел button в консоль

--- client/src/pages/jquery/main.js
+++ client/src/pages/jquery/main.js
@@ -1,12 +1,11 @@
 import $ from 'jquery'
-import moment from 'moment'
 
 export default function() {
     
     console.clear()
 
-    $("#button").click(function() {
-        var date = moment().format("DD.MM.YY HH:mm:ss")
-        $(".text").text(date)
+    $(".numbers").click(function() {
+        var html = $('.button').text();
+        console.log(html)
     })
 }
