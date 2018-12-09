3f8d335545a20e603f07a3c4614e34cafcbb4e7b
mrDobryak <clainrus@gmail.com>
2018-12-09 19:09:05 +1000

    Vue

---
+++ pages/todo/List.vue
@@ -0,0 +1,19 @@
+<template>
+    <ul>
+        <li v-for="item in list" :key="item.id">
+            <input type="checkbox" v-model="item.done">
+            <span>{{item.text}}</span>
+            <button @click="remove(item)">remove</button>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'todo-list',
+    props: {
+        list: Array,
+    }
+}
+</script>
+
--- pages/todo/todo.js
+++ pages/todo/todo.js
@@ -1,18 +1,24 @@
-import $ from 'jquery'
-import html from './todo.html'
+import Vue from 'vue'
+import Todo from './Todo.vue'
 
 export default function() {
-    $('#app').html(html)
-    $('#add').click(function() {
-        var text = $('#text').val()
-        var button = $('<button>-</button>').click(function() {
-            $(this).parent().remove()
-        })
-        var li = $('<li>'+text+'</li>').append(button)
-        $('#list').append(li)
-        $('#text').val('')
+    new Vue({
+        el: '#app',
+        render: function(h) {
+            return h(Todo)
+        }
     })
-    $('#clear').click(function() {
-        $('#list').empty()
-    })
+//     $('#app').html(html)
+//     $('#add').click(function() {
+//         var text = $('#text').val()
+//         var button = $('<button>-</button>').click(function() {
+//             $(this).parent().remove()
+//         })
+//         var li = $('<li>'+text+'</li>').append(button)
+//         $('#list').append(li)
+//         $('#text').val('')
+//     })
+//     $('#clear').click(function() {
+//         $('#list').empty()
+//     })
 }
