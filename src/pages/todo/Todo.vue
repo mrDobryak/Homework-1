c6f70cf5b6079be34d809b240900c1c05ad75027
mrDobryak <clainrus@gmail.com>
2019-01-13 19:07:38 +1000

    Add stylus

--- src/pages/todo/Todo.vue
+++ src/pages/todo/Todo.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <input v-model="input">
-        <button @click="add()">
+        <button class="add" @click="add()">
             add 
         </button>
         <ul>
@@ -9,11 +9,25 @@
                 {{item.text}}
             </li>
         </ul>
-        <button @click="clear()">
+        <button class="clear" @click="clear()">
             clear
         </button>
     </div>
 </template>
+
+<style scoped lang="stylus">
+.add
+    background-color black
+    color white
+    &:hover
+        background-color #888
+.clear
+    background-color blue
+    color white
+    &:hover
+        background-color #444
+</style>
+
 
 <script>
 export default {
@@ -24,36 +38,9 @@ export default {
             input: '',
         }
     },
-    created() {
-        this.addDefaultTask()
-        for (var i=0; i < this.list.length; i++ ){
-            console.log(this.list[i].text)
-        }
-    },
     methods: {
-        addDefaultTask() {
-            
-            this.list.push({
-                text: 'Default task',
-                id: 0,
-            })
-            
-            this.list.push({
-                text: 'Not Default task',
-                id: 1,
-            })
-            
-            this.list.push({
-                text: 'Not not Default task',
-                id: 2,
-            })
-            this.list.push({
-                text: 'Not12313 not Default task',
-                id: 3,
-            })
-            
-        },
         add() {
+            if (!this.input) return
             this.list.push({
                 text: this.input,
                 id: this.list.length + 1,
@@ -62,6 +49,7 @@ export default {
         },
         clear() {
             this.list = []
+            this.input = ""
         }
     },
 }
